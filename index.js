9af62f2a506c46b8e12507f37b95bf9a75d4bee3
Filippa Högnäsbacka <153224468+filippahognasbacka@users.noreply.github.com>
2024-03-27 13:10:36 +0200

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function deleteReview(reviewId) {
-    fetch("/delete-review", {
-        method: 'POST',
-        body: JSON.stringify({ reviewId: reviewId }),
-    })
-    .then(function(_res) {
-        window.location.href = "/";
-    });
-}
